8a8fe884bc5ad82316b757b77ba5d60491812cd9
YujaHey <119145741+YujaHey@users.noreply.github.com>
2024-04-21 14:03:24 -0700

    edited about.html

--- static/js/animate.js
+++ static/js/animate.js
@@ -1,5 +1,19 @@
 jQuery(document).ready(function($) {
-
+	function myFunction() {
+		var dots = document.getElementById("dots");
+		var moreText = document.getElementById("more");
+		var btnText = document.getElementById("myBtn");
+	  
+		if (dots.style.display === "none") {
+		  dots.style.display = "inline";
+		  btnText.innerHTML = "Read more";
+		  moreText.style.display = "none";
+		} else {
+		  dots.style.display = "none";
+		  btnText.innerHTML = "Read less";
+		  moreText.style.display = "inline";
+		}
+	}
 							
 	//animate effect	
 	$(".e_flash").hover(
